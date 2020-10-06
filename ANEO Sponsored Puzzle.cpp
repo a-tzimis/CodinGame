a5d42f361bac8240299d8eaa3796efeee9d6c92f
a-tzimis <65569566+a-tzimis@users.noreply.github.com>
2020-10-07 02:09:51 +0300

    Update ANEO Sponsored Puzzle.cpp

--- ANEO Sponsored Puzzle.cpp
+++ ANEO Sponsored Puzzle.cpp
@@ -11,7 +11,7 @@ int main()
     scanf("%d", &light_count);
     float dis[light_count], dur[light_count], k, begin = speed, test = 0;
     bool b = true;
-
+    
     for (int i = 0; i < light_count; i++)
     {
         int distance;
@@ -20,17 +20,15 @@ int main()
         dis[i] = distance;//saving distance in array
         dur[i] = duration;//saving duration in array
     }
- 
     for (int i = 0; i < light_count; i++) 
     {
         while(1)
         {
-            test+= (dur[i] * (speed * 1000)) / 3600;//finding how far the car can go with the current speed
-            counter++;//a counter to know when the light is green or not
-
-            if(test > dis[i] + 1)//if the travel is more than the distance + 1 line 31
+            test+= (dur[i] * (speed * 1000)) / 3600;
+            counter++;
+            if(test > dis[i] + 1)
             {
-                if(counter % 2 != 0)//check if the current light is green or not if it is not then take the initial speed--
+                if(counter % 2 != 0)
                 {
                     speed = begin - 1;
                     begin = speed;
@@ -42,8 +40,6 @@ int main()
         counter = 1;
         test = 0;
     }
-
     printf("%d",speed);
-
     return 0;
 }
